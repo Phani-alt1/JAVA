0b62eddb5bdb13723eb8013644167b07dee01b43
Phani-alt1 <75060953+Phani-alt1@users.noreply.github.com>
2024-07-10 09:57:04 +0530

    Create cube and square.java

---
+++ cube and square.java
@@ -0,0 +1,9 @@
+class cubeandsquare{
+    public static void main(String[] args) {
+        double a=0.6;
+        double square=a*a;
+        double cube=a*a*a;
+        System.out.println(square);
+        System.out.println(cube);
+    }
+}
