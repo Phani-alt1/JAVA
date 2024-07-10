f7464eb44df1f28aef1c6d36ff9b45e590657338
Phani-alt1 <75060953+Phani-alt1@users.noreply.github.com>
2024-07-10 09:14:41 +0530

    Create skip n numbers.java

---
+++ skip n numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class skip{
+    public static void main(String[] args){
+        int n=50;
+        int m=100;
+        int k=7;
+        for(int i=n;i<=m;i+=7){
+            System.out.println(i);
+        }
+    }
+}
