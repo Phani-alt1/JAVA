c206beedb45f42ebc9368bae8cd5b5682e9e38e6
Phani-alt1 <75060953+Phani-alt1@users.noreply.github.com>
2024-07-11 12:52:02 +0530

    Create patternnumber.java

---
+++ patternnumber.java
@@ -0,0 +1,13 @@
+class NumberPattern {
+    public static void main(String[] args) {
+        int row=4;
+        int num=1;
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num*num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
